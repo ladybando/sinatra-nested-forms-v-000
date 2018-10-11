b0b5cbc61a38c617f999c5c666c57f1c96fb4dee
Ayana Bando <ayanaflax@yahoo.com>
2018-10-11 13:54:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ module FormsLab
     end
     
     post '/pirates' do
-      @pirate = Pirates.new(params[:pirate])
+      @pirate = Pirate.new(params[:pirate])
      
       params[:pirate][:ships].each do |details|
         Ship.new(details)
